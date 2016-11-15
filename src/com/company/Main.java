52ad4b59aa1039513b82c3ab11f9f9f04ea11638
Nawaz Khan <nawazkhan@nawazs-macbook.apollosrc.root>
2016-11-15 11:03:29 -0600

    Day 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Person p = new Person();
-        p.name = "Alice";
-        p.age = 30;
-        p.isAlive = true;
+        Person.isValidName("Bob Johnson");
+
+        System.out.println(Person.hair);
+
+
 
     }
 }
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -4,8 +4,41 @@ package com.company;
  * Created by nawazkhan on 11/14/16.
  */
 public class Person {
-    String name;
-    int age;
-    boolean isAlive;
+    private String name;
+    private int age;
+    private boolean isAlive;
+
+    public static String hair = "Brown";
+
+    public Person() {
+        name = "Kyle";
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String n) {
+        if(isValidName(n)) {
+            name = n;
+        }
+    }
+
+    public static boolean isValidName(String name) {
+        return name.contains(" ") && !name.isEmpty() && name.length() <= 20;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int a) {
+        if(a > 0) {
+            age = a;
+        }
+    }
+    public boolean isAlive() {
+        return isAlive;
+    }
 
 }
